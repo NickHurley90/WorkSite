9effeb98bcfbdfe99be5443f10773bede3af7aab
NickHurley90 <nickolaushurley@gmail.com>
2024-10-09 18:52:28 -0400

    completed project4

--- main_app/views.py
+++ main_app/views.py
@@ -92,8 +92,8 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log in the user after signing up
-            return redirect('home')  # Redirect to the home page or another page
+            login(request, user)  
+            return redirect('home')  
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
